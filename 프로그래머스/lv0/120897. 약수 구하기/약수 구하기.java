5ec1d7ab7192f17cc736ae677527a07544ec5735
eoeo2255 <115970794+eoeo2255@users.noreply.github.com>
2023-03-28 10:45:25 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n) {
+        int count =0;
+        for (int i=1; i<=n; i++) {
+            if(n%i ==0) {
+                count++;
+            }
+        }
+        int[] answer = new int[count];
+        for (int i=1, j=0; i<=n; i++) {
+            if(n%i ==0) {
+                answer[j] = i;
+                j++;
+            }
+        }
+        return answer;
+    }
+}
