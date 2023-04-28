3847cbbe2c8e74271d85860542e5458c6e91a7e8
eoeo2255 <115970794+eoeo2255@users.noreply.github.com>
2023-04-28 21:02:47 +0900

    [level 0] Title: 외계어 사전, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+       for (int i = 0; i < dic.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < spell.length; j++) {
+                if (dic[i].contains(spell[j])) {
+                    count++;
+                }
+            }
+
+            if (count == spell.length) {
+                return 1;
+            }
+        }
+        return 2;
+    }
+}
