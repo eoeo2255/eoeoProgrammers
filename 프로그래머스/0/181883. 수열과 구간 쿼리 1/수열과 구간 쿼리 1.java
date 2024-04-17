e7c25ab9b12037963ae69db6e907bc3b0c7a0356
eoeo2255 <115970794+eoeo2255@users.noreply.github.com>
2024-04-17 17:44:33 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.23 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            for (int j = query[0]; j <= query[1]; j++) {
+                arr[j]++;
+            }
+        }
+        return arr;
+    }
+}
