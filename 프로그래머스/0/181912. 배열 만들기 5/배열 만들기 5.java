77a56e71d2c53e19be5391982fc5cfd823a0f516
eoeo2255 <115970794+eoeo2255@users.noreply.github.com>
2024-04-25 16:08:36 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.65 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < intStrs.length; i++) {
+            int num = Integer.parseInt(intStrs[i].substring(s,s+l));
+            if (num>k){
+                answer.add(num);
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
